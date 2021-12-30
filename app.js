9af261be2572160515b43f849b8e341977d66af6
Tatsuki Kobayashi <tatsuki009@gmail.com>
2021-12-30 09:02:14 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Tatsuki!'));
+app.get('/', (req, res) => res.send('Hello Tatsuki! Good day!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
